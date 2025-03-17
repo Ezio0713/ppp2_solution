b5bd820d1fc38649d1ad5a6f474d7e2d8d137c5b
Ezio0713 <915712276@qq.com>
2025-03-17 17:23:09 +0800

    Update chop8.cpp

--- chop8/chop8.cpp
+++ chop8/chop8.cpp
@@ -1,6 +1,33 @@
-﻿#include <iostream>
+﻿
+#include"std_libx_facilities.h"
+#include "test.cpp";
+//可以重复声明，但不可以重复定义
+//double sqrt(double);
+//double sqrt(double);
+////如果定义两个，那么下运行时报错
+////double sqrt(double) {
+////
+////}
+//double sqrt(double) {
+//	return 0;
+//	}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	//nameAge();
+	//vector<string> s = { "fe","aa","gewgew","gewa","hteh","bve" };
+	//for (string str :s) {
+	//	cout << str<<endl;
+	//}
+
+	//vector<int> c = getStringLength(s);
+	int i = 5;
+	constexpr int j = 6;
+
+	f(i);
+	f(j);
+	f(7);
+
+	return 0;
 }
 
